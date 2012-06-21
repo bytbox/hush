a5a584417945f52fcf39d09927774a6a4f331860
Scott Lawrence <bytbox@gmail.com>
2012-06-21 06:59:05 -0700

    Misc networking improvements

--- hush.c
+++ hush.c
@@ -1,12 +1,19 @@
+// Standard Libraries
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+// Networking
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
+
+// Audio
+// #include <alsa/asoundlib.h>
+// http://www.suse.de/~mana/alsa090_howto.html
 
 #ifndef DEFAULT_PORT
 #define DEFAULT_PORT "63212"
@@ -14,14 +21,15 @@
 
 #define EXIT(x) eval = x; goto _exit
 
-// TODO
-// #include <alsa/asoundlib.h>
-// http://www.suse.de/~mana/alsa090_howto.html
+// Flags for data.
+#define SOUND (1 << 1)
 
 char *pname;
 
 int client(const char *, const char *);
 int server(const char *);
+
+int send_data(int, char, short, const char *);
 
 int main(int argc, char *argv[]) {
 	pname = argv[0];
@@ -65,14 +73,22 @@ int client(const char *host, const char *port) {
 	int sockfd = socket(ai->ai_family, SOCK_STREAM, 0);
 	if (sockfd < 0) {
 		fprintf(stderr, "%s: socket: %s\n", pname, strerror(errno));
+		EXIT(EXIT_FAILURE);
+	}
+	int ndflag = 1;
+	if (setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &ndflag, sizeof(ndflag))) {
+		fprintf(stderr, "%s: setsockopt: %s\n", pname, strerror(errno));
+		EXIT(EXIT_FAILURE);
 	}
 	if (connect(sockfd, ai->ai_addr, ai->ai_addrlen) < 0) {
 		fprintf(stderr, "%s: connect: %s\n", pname, strerror(errno));
 		EXIT(EXIT_FAILURE);
 	}
 
-	FILE *sock = fdopen(sockfd, "rw");
-	fclose(sock);
+	if (send_data(sockfd, 0, 5000, "hellot") < 0) {
+		fprintf(stderr, "%s: send_data: %s\n", pname, strerror(errno));
+		EXIT(EXIT_FAILURE);
+	}
 
 _exit:
 	if (ai) freeaddrinfo(ai);
@@ -86,26 +102,50 @@ int server(const char *port) {
 		fprintf(stderr, "%s: getaddrinfo: %s\n", pname, gai_strerror(en));
 		EXIT(EXIT_FAILURE);
 	}
-
+	
 	int sockfd = socket(ai->ai_family, SOCK_STREAM, 0);
 	if (sockfd < 0) {
 		fprintf(stderr, "%s: socket: %s\n", pname, strerror(errno));
+		EXIT(EXIT_FAILURE);
+	}
+	int ndflag = 1;
+	if (setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &ndflag, sizeof(ndflag))) {
+		fprintf(stderr, "%s: setsockopt: %s\n", pname, strerror(errno));
 		EXIT(EXIT_FAILURE);
 	}
 	if (bind(sockfd, ai->ai_addr, ai->ai_addrlen) < 0) {
 		fprintf(stderr, "%s: bind: %s\n", pname, strerror(errno));
 		EXIT(EXIT_FAILURE);
 	}
-	if (listen(sockfd, 0)) {
+	if (listen(sockfd, 0) < 0) {
+		fprintf(stderr, "%s: listen: %s\n", pname, strerror(errno));
+		EXIT(EXIT_FAILURE);
+	}
+	int fd = accept(sockfd, NULL, NULL);
+	if (fd < 0) {
 		fprintf(stderr, "%s: listen: %s\n", pname, strerror(errno));
 		EXIT(EXIT_FAILURE);
 	}
 
-	FILE *sock = fdopen(sockfd, "rw");
+	FILE *sock = fdopen(fd, "rw");
 	fclose(sock);
 
 _exit:
 	if (ai) freeaddrinfo(ai);
 	return eval;
+}
+
+int send_data(int fd, char flags, short len, const char *data) {
+	/* The format of the data sent is:
+	 *   1   flags
+	 *   2   length (in qwords)
+	 *   3-4 other
+	 *   5+  data
+	 */
+	if (len >= (1 << 16)) { // Send no more than 64 KB of data.
+		errno = EOVERFLOW;
+		return -1;
+	}
+	return 0;
 }
 
